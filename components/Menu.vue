23a4ca114d86f71470da1bfc11353588ea5e3a5f
r-matsumoto <r-matsumoto@kdl.co.jp>
2022-05-15 12:23:13 +0900

    fix: Changed headless ui to be built only on the client

--- components/Menu.vue
+++ components/Menu.vue
@@ -32,82 +32,88 @@ watchEffect(() => {
 </script>
 
 <template>
-  <Menu as="div" class="relative">
-    <MenuButton as="template">
-      <div
-        class="h-3 w-8 border-t border-b border-black dark:border-white cursor-pointer"
-      ></div>
-    </MenuButton>
-    <MenuItems as="template">
-      <div
-        class="absolute right-0 mt-2 rounded-md shadow-md p-4 w-64 max-w-screen-sm bg-white dark:bg-black z-50"
-      >
-        <MenuItem as="div">
-          <div>
-            <span class="text-xs text-gray-400 mb-2 inline-block">テーマ</span>
-            <div class="mx-auto w-full max-w-md">
-              <RadioGroup v-model="selected">
-                <RadioGroupLabel class="sr-only">Server size</RadioGroupLabel>
-                <div class="space-y-2">
-                  <RadioGroupOption
-                    as="template"
-                    v-for="theme in themes"
-                    :key="theme.name"
-                    :value="theme"
-                    v-slot="{ active, checked }"
-                  >
-                    <div
-                      :class="[
-                        active
-                          ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-gray-300'
-                          : '',
-                        checked
-                          ? 'bg-gray-900 bg-opacity-75 text-white '
-                          : 'bg-white ',
-                      ]"
-                      class="relative flex cursor-pointer rounded-lg p-2 py-3 focus:outline-none"
+  <ClientOnly>
+    <Menu as="div" class="relative">
+      <MenuButton as="template">
+        <div
+          class="h-3 w-8 border-t border-b border-black dark:border-white cursor-pointer"
+        ></div>
+      </MenuButton>
+      <MenuItems as="template">
+        <div
+          class="absolute right-0 mt-2 rounded-md shadow-md p-4 w-64 max-w-screen-sm bg-white dark:bg-black z-50"
+        >
+          <MenuItem as="div">
+            <div>
+              <span class="text-xs text-gray-400 mb-2 inline-block"
+                >テーマ</span
+              >
+              <div class="mx-auto w-full max-w-md">
+                <RadioGroup v-model="selected">
+                  <RadioGroupLabel class="sr-only">Server size</RadioGroupLabel>
+                  <div class="space-y-2">
+                    <RadioGroupOption
+                      as="template"
+                      v-for="theme in themes"
+                      :key="theme.name"
+                      :value="theme"
+                      v-slot="{ active, checked }"
                     >
-                      <div class="flex w-full items-center justify-between">
-                        <div class="flex items-center">
-                          <CheckIcon :checked="checked" />
-                          <div class="text-sm">
-                            <RadioGroupLabel
-                              as="p"
-                              :class="checked ? 'text-white' : 'text-gray-900'"
-                              class="font-medium"
-                            >
-                              {{ theme.name }}
-                            </RadioGroupLabel>
-                            <RadioGroupDescription
-                              as="span"
-                              :class="
-                                checked ? 'text-gray-100' : 'text-gray-500'
-                              "
-                              class="inline text-xs"
-                            >
-                              <span>{{ theme.description }}</span>
-                            </RadioGroupDescription>
+                      <div
+                        :class="[
+                          active
+                            ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-gray-300'
+                            : '',
+                          checked
+                            ? 'bg-gray-900 bg-opacity-75 text-white '
+                            : 'bg-white ',
+                        ]"
+                        class="relative flex cursor-pointer rounded-lg p-2 py-3 focus:outline-none"
+                      >
+                        <div class="flex w-full items-center justify-between">
+                          <div class="flex items-center">
+                            <CheckIcon :checked="checked" />
+                            <div class="text-sm">
+                              <RadioGroupLabel
+                                as="p"
+                                :class="
+                                  checked ? 'text-white' : 'text-gray-900'
+                                "
+                                class="font-medium"
+                              >
+                                {{ theme.name }}
+                              </RadioGroupLabel>
+                              <RadioGroupDescription
+                                as="span"
+                                :class="
+                                  checked ? 'text-gray-100' : 'text-gray-500'
+                                "
+                                class="inline text-xs"
+                              >
+                                <span>{{ theme.description }}</span>
+                              </RadioGroupDescription>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </RadioGroupOption>
-                </div>
-              </RadioGroup>
+                    </RadioGroupOption>
+                  </div>
+                </RadioGroup>
+              </div>
             </div>
-          </div>
-        </MenuItem>
-        <template v-if="user">
-          <MenuItem
-            as="div"
-            class="mt-8 pt-4 border-t-2 border-gray-300 dark:border-gray-600"
-          >
-            <AppButton :onClick="logout" type="filled" class="w-full"
-              >ログアウト</AppButton
-            >
           </MenuItem>
-        </template>
-      </div>
-    </MenuItems>
-  </Menu>
+          <template v-if="user">
+            <MenuItem
+              as="div"
+              class="mt-8 pt-4 border-t-2 border-gray-300 dark:border-gray-600"
+            >
+              <AppButton :onClick="logout" type="filled" class="w-full"
+                >ログアウト</AppButton
+              >
+            </MenuItem>
+          </template>
+        </div>
+      </MenuItems>
+    </Menu>
+  </ClientOnly>
 </template>
