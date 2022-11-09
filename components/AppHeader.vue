6d18f17c60c25d242e386b32bd2d84ecacb0b83f
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-11-09 21:12:47 +0900

    feat: headerのUI修正

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="flex items-center justify-between w-full h-10">
-    <h1 class="font-oswald">Presentation Watch</h1>
+    <div class="flex items-center">
+      <img
+        src="/images/logo.svg"
+        alt="logo"
+        class="mr-2"
+        width="24"
+        height="24"
+      />
+      <h1 class="font-hiragino tracking-widest font-bold">つながるTIMER</h1>
+    </div>
     <Menu />
   </div>
 </template>
