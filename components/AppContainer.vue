f43ee7f6ab139584a11dd32be9de7f0e9cf9dc1a
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-11-09 21:55:59 +0900

    fix: ogpを扱えるように修正

--- components/AppContainer.vue
+++ components/AppContainer.vue
@@ -6,7 +6,7 @@ const url = "https://tsunagaru-timer.ryotanny.com";
 </script>
 
 <template>
-  <Html lang="ja">
+  <Html lang="ja" prefix="og: http://ogp.me/ns#">
     <Head>
       <Title>{{ title }}</Title>
       <Meta name="description" :content="description" />
