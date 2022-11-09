b5875435856130cc9c3b6cf00831077f0eccc7bf
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-11-09 21:12:31 +0900

    feat: headの修正

--- components/AppContainer.vue
+++ components/AppContainer.vue
@@ -28,67 +28,68 @@ const url = "https://presentation-timer.ryotanny.com";
         href="/apple-touch-icon.png"
       />
       <Link
-        rel="icon"
+        rel="icon alternate"
         type="image/png"
         size="32x32"
         href="/favicon-32x32.png"
       />
       <Link
-        rel="icon"
+        rel="icon alternate"
         type="image/png"
         size="16x16"
         href="/favicon-16x16.png"
       />
       <Link rel="manifest" href="/site.webmanifest" />
-      <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      <Link rel="icon alternate" type="image/x-icon" href="/favicon.ico" />
+      <link rel="icon" href="images/favicon.svg" type="image/svg+xml" />
       <!-- pwa（link） -->
       <Link
-        href="/splashscreens/iphone5_splash.png"
+        href="/iphone5_splash.png"
         media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/iphone6_splash.png"
+        href="/iphone6_splash.png"
         media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/iphoneplus_splash.png"
+        href="/iphoneplus_splash.png"
         media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/iphonex_splash.png"
+        href="/iphonex_splash.png"
         media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/iphonexr_splash.png"
+        href="/iphonexr_splash.png"
         media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/iphonexsmax_splash.png"
+        href="/iphonexsmax_splash.png"
         media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/ipad_splash.png"
+        href="/ipad_splash.png"
         media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/ipadpro1_splash.png"
+        href="/ipadpro1_splash.png"
         media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/ipadpro3_splash.png"
+        href="/ipadpro3_splash.png"
         media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
       <Link
-        href="/splashscreens/ipadpro2_splash.png"
+        href="/ipadpro2_splash.png"
         media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
         rel="apple-touch-startup-image"
       />
@@ -105,13 +106,20 @@ const url = "https://presentation-timer.ryotanny.com";
       />
     </Head>
     <div
-      class="bg-stone-50 text-stone-900 dark:bg-stone-900 dark:text-stone-50 min-h-screen"
+      class="bg-stone-50 text-stone-900 dark:bg-stone-900 dark:text-stone-50 min-h-screen min-h-screenDvh"
     >
       <div
-        class="container mx-auto px-4 min-h-screen flex flex-col items-center"
+        class="container mx-auto px-4 min-h-screen min-h-screenDvh flex flex-col items-center"
       >
         <slot />
       </div>
     </div>
   </Html>
 </template>
+
+<style>
+body {
+  font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN",
+    "Hiragino Sans", Meiryo, sans-serif;
+}
+</style>
