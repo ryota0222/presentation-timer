48eead2f0d5b50f16e78b31e654bd67355f4da97
Matsumoto Ryota <awpjdmga1943@icloud.com>
2022-11-09 21:11:13 +0900

    feat: 閲覧モードの作成

--- components/Timer.vue
+++ components/Timer.vue
@@ -2,6 +2,7 @@
 import { Action, LogFetchedDB } from "~/types/log";
 import { useStopwatch } from "vue-timer-hook";
 import useTimer from "~~/composables/useTimer";
+import { useModeSetting } from "~~/composables/useModeSetting";
 import useFireStore from "~~/composables/useFireStore";
 type Props = {
   offsetTimeStamp: number;
@@ -11,6 +12,7 @@ const props = defineProps<Props>();
 const AUTO_START = false;
 const logs = useState<LogFetchedDB[]>("logs", () => props.initialLog ?? []);
 const stopwatch = useStopwatch(props.offsetTimeStamp, AUTO_START);
+const { isReadOnlyMode } = useModeSetting();
 // logの更新
 const updateLog = (data: LogFetchedDB) => {
   logs.value.unshift(data);
@@ -74,37 +76,40 @@ watchEffect(() => {
       <div class="mb-8">
         <TimerIcon :isRunning="stopwatch.isRunning.value" />
       </div>
-      <!-- カウント中 -->
-      <template v-if="stopwatch.isRunning.value">
-        <AppButton
-          :onClick="() => handlePause(logs[0].created_at)"
-          type="filled"
-          >ストップ</AppButton
-        ><br />
-      </template>
-      <!-- カウント停止中 -->
-      <template v-else>
-        <!-- 00:00:00:00の時 -->
-        <template
-          v-if="
-            stopwatch.days.value === 0 &&
-            stopwatch.hours.value === 0 &&
-            stopwatch.minutes.value === 0 &&
-            stopwatch.seconds.value === 0
-          "
-          ><AppButton :onClick="handleStart" type="filled">スタート</AppButton
-          ><br
-        /></template>
+      <template v-if="!isReadOnlyMode">
+        <!-- カウント中 -->
+        <template v-if="stopwatch.isRunning.value">
+          <AppButton
+            :onClick="() => handlePause(logs[0].created_at)"
+            type="filled"
+            >ストップ</AppButton
+          ><br />
+        </template>
+        <!-- カウント停止中 -->
         <template v-else>
-          <div class="flex-col space-y-4 inline-flex">
-            <AppButton :onClick="handleRestart" type="filled">再開</AppButton>
-            <AppButton :onClick="handleReset" type="outlined"
-              >リセット</AppButton
-            >
-          </div>
+          <!-- 00:00:00:00の時 -->
+          <template
+            v-if="
+              stopwatch.days.value === 0 &&
+              stopwatch.hours.value === 0 &&
+              stopwatch.minutes.value === 0 &&
+              stopwatch.seconds.value === 0
+            "
+            ><AppButton :onClick="handleStart" type="filled">スタート</AppButton
+            ><br
+          /></template>
+          <template v-else>
+            <div class="flex-col space-y-4 inline-flex">
+              <AppButton :onClick="handleRestart" type="filled">再開</AppButton>
+              <AppButton :onClick="handleReset" type="outlined"
+                >リセット</AppButton
+              >
+            </div>
+          </template>
         </template>
       </template>
     </div>
+    <!-- !ログ一覧：開発中のみ利用 -->
     <!-- <ul v-if="logs.length">
       <li v-for="log of logs" :key="log.id">
         {{ log.created_at }}: {{ log.action }}
